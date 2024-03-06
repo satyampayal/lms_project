a59084c4faaa22b6f4d13416d1c462b80360809c
satyam <satyampayal10@gmail.com>
2024-03-06 20:19:59 +0530

    React-hot-toast

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
-
+import { useEffect } from 'react'
+import { toast } from 'react-hot-toast'
 function App() {
+  useEffect(()=>{
+  //  toast.error('Hello',{
+  //   duration:10000,
+  //   position:'top-center'
+  //  })
+  toast.success('Confirm');
+  })
   return (
     <div className='text-6xl'>This is starting</div>
   )
