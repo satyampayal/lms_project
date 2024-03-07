892f06cef915def9947c12d399bf0d331462f0bb
satyam <satyampayal10@gmail.com>
2024-03-07 22:04:33 +0530

    footer developed

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -4,11 +4,13 @@ function Footer() {
     const newDate = new Date();
     const year = newDate.getFullYear();
     return (
-        <footer className=' relative left-0 bottom-0 lg:md:h-[10vh] sm:h-[20vh]  py-5 bg-gray-800 text-white flex flex-col sm:flex-row items-center ' >
+        <footer className='relative left-0 bottom-0  lg:md:h-[5vh] sm:h-[10vh]  py-5 bg-gray-800
+                             text-white flex flex-col sm:flex-row items-center ' >
             <section className=' m-4 sm:text-[16px] text-lg '>
                 Copyright {year} | All rights reserved
             </section>
-            <section className='justify-end flex items-center lg:md:justify-end sm:justify-center text-2xl   gap-5'>
+            <section className=' grid grid-cols-4 items-center justify-end
+                                 sm:justify-center text-2xl   gap-5'>
                 <a href="#" className='hover:scale-[1.3] '>
                     <BsFacebook />
                 </a>
